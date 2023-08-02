1d6b57df43cb53e8d5875d462d4ca6feaf99f902
jose20pavas <jomapago05@gmail.com>
2023-08-01 23:31:18 -0500

    ejercio1 hasta ejercicio 3 agregados

---
+++ Ejercicio_61_JosePavas.py
@@ -0,0 +1,18 @@
+import random
+
+
+nombre=input("ingrese su nombre")
+edad=int(input("ingrese su edad"))
+
+def area ():
+    radio=int(input("ingrese el radio del circulo"))
+    return 3.14*radio**2
+
+print(area())
+
+
+num=random.randint(0,10000)
+print(num)
+
+
+
